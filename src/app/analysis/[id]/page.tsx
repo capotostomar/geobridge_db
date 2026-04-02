ed5d90dfd1b2a1e87acb731c7d93ad4ef4c10404
capotostomar <151038597+capotostomar@users.noreply.github.com>
2026-04-02 20:28:36 +0200

    Delete src directory

--- src/app/analysis/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { use } from 'react'
-import { AnalysisPage } from '@/components/analysis/analysis-page'
-
-export default function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = use(params)
-  return <AnalysisPage id={id} />
-}
